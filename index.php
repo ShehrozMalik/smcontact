811dd439f4ef39c26864a192401252428d5c3b6d
unknown <shehrozmalik>
2017-07-29 01:13:09 +0500

    this is my first commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>SM Contacts</title>
+
+		<link rel="stylesheet" href="assets/style.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+	<header>
+	
+
+	<div class="jumbotron">
+	<div class="container">
+	<h1 class="heading text-center">SM CONTACTS</h1>
+
+	</div>
+	</div>
+
+
+	</header>
+		<div class="container">
+		<div class="row">
+			<div class="col-xs-3">
+				<aside class="left-side">
+					<li><a href="index.php">Welcome!</a></li>
+					<li><a href="add-record.php">Add Record</a></li>
+					<li><a href="view-record.php">View Record</a></li>
+					<li><a href="#">Search Record</a></li>
+					<li><a href="#">Trashcan</a></li>
+					<li><a href="#">Logout</a></li>
+				</aside>	
+				
+			</div>	
+			<div class="col-xs-9">
+			<div class="right-side">
+			<div class="main-content">
+		
+				<h3>Welcome!</h3>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived no It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+
+</p>
+<button type="button" class="btn btn-default"><a href="login.php"><i class="fa fa-user"></i> Login</a></button>
+<button type="button" class="btn btn-default"><a href="register.php"><i class="fa fa-sign-in"></i> Register</a></button>
+</div>
+			</div>
+
+				
+			</div>
+		</div>
+		</div>
+
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="Hello World"></script>
+	</body>
+</html>
---
+++ login.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>SM Contacts</title>
+
+		<link rel="stylesheet" href="assets/style.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+	<header>
+	
+
+	<div class="jumbotron">
+	<div class="container">
+	<h1 class="heading text-center">SM CONTACTS</h1>
+
+	</div>
+	</div>
+
+
+	</header>
+		<div class="container">
+		<div class="row">
+			<div class="col-xs-3">
+				<aside class="left-side">
+					<li><a href="index.php">Welcome!</a></li>
+					<li><a href="add-record.php">Add Record</a></li>
+					<li><a href="view-record.php">View Record</a></li>
+					<li><a href="#">Search Record</a></li>
+					<li><a href="#">Trashcan</a></li>
+					<li><a href="#">Logout</a></li>
+				</aside>	
+				
+			</div>	
+			<div class="col-xs-9">
+			<div class="right-side">
+			<div class="main-content">
+
+				<div class="form-group">
+				<a href="index.php"><i class="fa fa-arrow-left"></i></a>
+					<legend>Login</legend>
+				</div>
+		
+				<form action="" method="POST" class="form-horizontal" role="form">
+				
+					<div class="form-group">
+					
+						<input type="text" class="form-control" id="" placeholder="User Name">
+						
+					</div>
+				
+					<div class="form-group">
+						<input type="password" class="form-control" id="" placeholder="Password">
+					</div>
+					
+				
+					<button type="submit" class="btn btn-primary">Submit</button>
+					<a href="#">forgot password</a>
+				</form>
+		
+
+</div>
+		
+				
+</div>
+			</div>
+
+				
+			</div>
+		</div>
+		</div>
+
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="Hello World"></script>
+	</body>
+</html>
